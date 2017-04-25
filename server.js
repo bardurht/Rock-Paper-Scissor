14d9da232b0dc3198eba5b1fed1b4eefc79ecb30
Bardur <bardurthomsen@hotmail.com>
2017-04-25 13:35:38 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,60 @@
+var express = require("express");
+var app = express();
+
+var bodyParser = require("body-parser");
+
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded( {"extended":false} ) );
+
+// Create some players for testing
+var jPlayerOne = {"nickname":"a","choice":""};
+var jPlayerTwo = {"nickname":"b","choice":""};
+
+
+
+
+app.get( "/game", function( req,res ){
+	res.sendFile(__dirname+"/game.html");
+});
+
+
+app.get( "/match/:nickname/:choice" , function( req , res ){
+	var sNickname = req.params.nickname;
+	var sChoice 	= req.params.choice;
+	console.log( sNickname , sChoice );
+	if( sNickname == "a" ){
+		jPlayerOne.choice =  sChoice;
+	}else{
+		jPlayerTwo.choice =  sChoice;
+	}
+	res.json({"response":"ok"});
+});
+
+
+
+
+app.get( "/result/:nickname" , function( req , res ){
+	var sNickname = req.params.nickname;
+
+	// WAITING
+	if( jPlayerOne.choice == "" || jPlayerTwo.choice == "" ){
+		res.json({"status":"wait"});
+	}
+	// EVEN
+	if( jPlayerOne.choice == jPlayerTwo.choice  ){		
+		res.json( {"status":"even"} );
+	}
+	// WINNER
+	if( jPlayerOne.choice == "rock" && jPlayerTwo.choice == "scissor" ){
+		res.json( {"status":"winner","nickname":jPlayerOne.nickname} );
+	}
+
+
+
+});
+
+
+
+app.listen(80, function(){
+	console.log("SERVER RUNNING");
+});
